f1b06c14de31b9fe971b87a1607e5333047d2b7c
werbyvillarino <werbyvillarino@gmail.com>
2020-10-18 09:51:28 +0000

    changes commited

--- program.py
+++ program.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-def create_app():
-	app = Flask(__name__, static_url_path="", static_folder="static")
+db = SQLAlchemy()
 
+def create_app():
+	app = Flask(__name__,static_url_path="", static_folder="static")
+	app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://wdvsample:qwerty+456@db4free.net/wdvsampledb"
+	
+	db.init_app(app)
+	
 	with app.app_context():
 		from mainroutes import routes
 		app.register_blueprint(routes)
 
-		return app
+		db.create_all()
+		
